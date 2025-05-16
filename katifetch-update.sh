2a8b62b7a303ce81acb348956b90938f90798e9c
ximimoments <161499192+ximimoments@users.noreply.github.com>
2025-05-16 13:03:12 -0300

    Update katifetch-update.sh

--- katifetch-update.sh
+++ katifetch-update.sh
@@ -2,7 +2,7 @@
 
 echo "Updating Katifetch..."
 cd ~ || exit
-git clone --depth=1 https://github.com/tu-usuario/katifetch katifetch-tmp
+git clone --depth=1 https://github.com/ximimoments/katifetch katifetch-tmp
 cp -r katifetch-tmp/* ~/katifetch/
 rm -rf katifetch-tmp
 echo "Update completed."
