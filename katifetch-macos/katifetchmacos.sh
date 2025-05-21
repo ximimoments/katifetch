84541fd71277b918a42e9d3dfabb1a215f9b9fc3
ximimoments <161499192+ximimoments@users.noreply.github.com>
2025-05-20 21:33:04 -0300

    Update katifetchmacos.sh

--- katifetch-macos/katifetchmacos.sh
+++ katifetch-macos/katifetchmacos.sh
@@ -1,20 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# katifetch for macOS – Apple logo with color, centered text
+# ANSI colors
+magenta='\033[1;35m'
+white='\033[1;37m'
+reset='\033[0m'
 
-# Define colors
-PINK="\033[1;35m"
-RESET="\033[0m"
-
-# Detect if running on macOS
-IS_MAC=false
-if [[ "$(uname)" == "Darwin" ]]; then
-  IS_MAC=true
-fi
-
-# Apple ASCII logo (DO NOT EDIT)
-apple_logo=$(cat << "EOF"
-                 ,xNMM.
+# macOS ASCII logo
+logo="${magenta}                 ,xNMM.
                .OMMMMo
                lMM\"
      .;loddo:.  .olloddol;.
@@ -29,46 +21,25 @@ apple_logo=$(cat << "EOF"
   'XMMMMMMMMMMMMMMMMMMMMMMMMK.
     kMMMMMMMMMMMMMMMMMMMMMMd
      ;KMMMMMMMWXXWMMMMMMMk.
-       "cooc*"    "*coo'"
-EOF
-)
+       \"cooc*\"    \"*coo'\"${reset}"
 
-# Apply pink color only if on macOS
-if $IS_MAC; then
-  apple_logo="$(echo "$apple_logo" | sed "s/^/${PINK}/")"
-fi
-
-# Get system info
-os_name=$(sw_vers -productName)
+# System info
+os=$(sw_vers -productName)
 os_version=$(sw_vers -productVersion)
-hostname=$(hostname)
 kernel=$(uname -r)
+hostname=$(hostname)
 shell=$(basename "$SHELL")
-terminal="${TERM_PROGRAM:-Unknown}"
+terminal="$TERM_PROGRAM"
 
-# Create info block and center it
-title="katifetch"
-info=$(cat << EOF
-$os_name $os_version
+# Info block
+info="${white}katifetch
+
+OS: $os $os_version
 Hostname: $hostname
 Kernel: $kernel
 Shell: $shell
-Terminal: $terminal
-EOF
-)
+Terminal: $terminal${reset}"
 
-# Center helper
-center() {
-  local term_width=$(tput cols)
-  while IFS= read -r line; do
-    printf "%*s\n" $(( (${#line} + term_width) / 2 )) "$line"
-  done
-}
-
-# Print logo + info
-echo -e "$apple_logo${RESET}"
-echo
-center <<< "$title"
-echo
-center <<< "$info"
+# Print logo and info
+printf "%s\n\n%s\n" "$logo" "$info"
 
